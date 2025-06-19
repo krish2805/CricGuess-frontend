3b0a26939793e044af251045015d083c6b33af02
krish2805 <krishchaudhari2805@gmail.com>
2025-06-19 10:01:38 +0530

    change in code

--- src/utils/cricketPredictor.js
+++ src/utils/cricketPredictor.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://cricguess-dg3w.onrender.com/api/";
+const API_BASE = "https://cricguess-dg3w.onrender.com/api";
 
 export const fetchPlayerClues = async (answersArray) => {
   try {
